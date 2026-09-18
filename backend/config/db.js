cfc9972f492e566d2393d0e2f1e8c8c53ad2a745
Angel <angel15laverde16@gmail.com>
2026-09-18 11:06:27 -0500

    configuracion para despliegue prueba 1

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,21 @@
+require("dotenv").config({ path: __dirname + "/../dbPassword.env" });
+const fs = require("fs");
+const path = require("path");
+
 const mysql = require("mysql2");
 
 // Configuración de la conexión a la base de datos única
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "sqlCuentasdj",
+  host: "mysql-9788a4f-angel15laverde16-4aae.b.aivencloud.com",
+  port: 21715,
+  user: "avnadmin",
+  password: process.env.PASSWORD,
   database: "cuentasdj",
-  timezone: `local`,
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, "..", "ca.pem")),
+    rejectUnauthorized: true,
+  },
+  debug: true,
 });
 
 // Conexión a la base de datos y manejo de errores
@@ -14,6 +23,8 @@ db.connect((err) => {
   if (err) {
     console.error("Error al conectar a la base de datos:", err);
     return;
+  } else {
+    console.log("conexion correcta");
   }
 });
 
